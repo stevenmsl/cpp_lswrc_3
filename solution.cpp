0b13eb9dac9a251da8d58bb4e2bd72ce5a9340bd
Steven Lin <steven_lx@outlook.com>
2022-06-14 09:43:23 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -16,6 +16,14 @@ using namespace std;
 /*takeaways
   - the basic idea here is to use a sliding window
     to determine the length of the substring.
+  - use a hash table to remember the last index
+    of each char
+    - each index is set to -1 so we can add different
+      char to the window without shrinking the window
+  - calculate the new start of the window each time
+    you scan in a char
+    -  start = max(start, lastIndx[s[j]] + 1);
+
   - take abca as an example.
     - the window will first grow to "abc" and then we when scan in the next 'a' char
       we know it will cause a repeat of 'a' in the substring so we have to slide
@@ -24,9 +32,11 @@ using namespace std;
     - the question here is how can we determine there is a repeat
       quickly to achieve O(n)?
       - remember the last index of each char, and look for the index of the next char
-      - for 'a' the last index of it is 0 when you are scanning in the 'a' at position 3,
+      - for 'a', the last index is 0 when you are scanning in the 'a' at position 3,
         so 0 (last index) + 1 (next position) = 1 which should be the new start
-        for the window after it slid.
+        for the window, after it slid, if there are no repeating chars between
+        0 and 3.
+
       - consider another case "abba", when you are scanning the next 'a' at position 3,
         you would conclude that the new start of the window should be 0 + 1 = 1,
         however, before that we have updated the start of the window to 2 already while
@@ -48,8 +58,13 @@ int Solution::lengthOf(string s)
   for (int start = 0, j = 0; j < n; j++)
   {
     /* set the new start of the window
-       - use max to handle the case where there
-         are repeats before s[j]
+       - shrink the window to the right
+         by 1 to remove the repeating
+         chars
+       - but the window might have moved
+         already if there are repeating
+         chars between s[lastIndex]..s[j]
+       - use max to find the right start
     */
     start = max(start, lastIndx[s[j]] + 1);
     maxLen = max(maxLen, j - start + 1);
